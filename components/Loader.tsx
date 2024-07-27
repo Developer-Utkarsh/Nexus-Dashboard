9b9ab2e075e6392c324b79dd7edf15df656f8089
Utkarsh Tiwari <utkarshweb2023@gmail.com>
2024-07-27 17:43:18 +0530

    added suspense loader in auth

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Loader = () => {
-	return <span className='loader'></span>;
+const Loader = ({ className }: { className?: string }) => {
+	return <span className={` ${className ? "loader-lg" : "loader"}`}></span>;
 };
 
 export default Loader;
