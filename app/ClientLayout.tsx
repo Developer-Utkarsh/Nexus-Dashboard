18de73da80770ce8d0cf30be42262714b1f2d39b
Utkarsh Tiwari <utkarshweb2023@gmail.com>
2024-08-02 17:37:37 +0530

    admin verfication: bug fixed

--- app/ClientLayout.tsx
+++ app/ClientLayout.tsx
@@ -79,7 +79,7 @@ function AdminCheck({ children }: { children: React.ReactNode }) {
 		checkAdminStatus();
 	}, [user]);
 
-	if (loading) {
+	if (loading || isAdmin === null) {
 		return (
 			<div className='bg-black h-screen w-full flex justify-center items-center'>
 				<Loader className='loader-lg' />
